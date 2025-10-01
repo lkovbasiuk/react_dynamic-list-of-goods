4f7592f585c34efa08f0d4fcc3f6955d41add957
Liudmyla Kovbasiuk <lchubatiuk96@gmail.com>
2025-10-01 10:26:13 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -7,46 +7,63 @@ import { Good } from './types/Good';
 
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const handleErrors = () => {
+    setIsLoading(true);
+    getAll()
+      .then((data: Good[]) => {
+        setGoods(data);
+      })
+      .catch(() => setError('Try again later'))
+      .finally(() => setIsLoading(false));
+  };
 
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
-      <button
-        onClick={() => {
-          getAll().then((data: Good[]) => {
-            setGoods(data);
-          });
-        }}
-        type="button"
-        data-cy="all-button"
-      >
-        Load all goods
-      </button>
+      {isLoading && <p> ... </p>}
 
-      <button
-        onClick={() => {
-          get5First().then((data: Good[]) => {
-            setGoods(data);
-          });
-        }}
-        type="button"
-        data-cy="first-five-button"
-      >
-        Load 5 first goods
-      </button>
+      {!isLoading && (
+        <>
+          <button
+            onClick={() => {
+              handleErrors();
+            }}
+            type="button"
+            data-cy="all-button"
+          >
+            Load all goods
+          </button>
 
-      <button
-        onClick={() => {
-          getRedGoods().then((data: Good[]) => {
-            setGoods(data);
-          });
-        }}
-        type="button"
-        data-cy="red-button"
-      >
-        Load red goods
-      </button>
+          <button
+            onClick={() => {
+              get5First().then((data: Good[]) => {
+                setGoods(data);
+              });
+            }}
+            type="button"
+            data-cy="first-five-button"
+          >
+            Load 5 first goods
+          </button>
+
+          <button
+            onClick={() => {
+              getRedGoods().then((data: Good[]) => {
+                setGoods(data);
+              });
+            }}
+            type="button"
+            data-cy="red-button"
+          >
+            Load red goods
+          </button>
+        </>
+      )}
+
+      {error && <p>{error}</p>}
 
       <GoodsList goods={goods} />
     </div>
